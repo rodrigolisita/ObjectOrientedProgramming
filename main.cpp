a94435ed1899eb24c959ee637ed9118adf9e1f29
Rodrigo Lisita Ribera <rodrigolisita@gmail.com>
2024-09-11 10:53:27 -0300

    Print menu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 
 int main()
 {
-    std::cout << "Hello BSc CS" << std::endl;
+    // 1 print help
+    std::cout << "1: Print help " << std::endl;
+
+    // 2 print exhange stats
+    std::cout << "2: Print exhange stats " << std::endl;
+    
+    // 3 make an offer
+    std::cout << "3: Make an offer " << std::endl;
+    
+    // 4 make a bid
+    std::cout << "4: Make a bid " << std::endl;
+    
+    // 5 print wallet
+    std::cout << "5: Print wallet " << std::endl;
+    
+    // 6 continue
+    std::cout << "6: Continue " << std::endl;
+
     return 0;
 }
