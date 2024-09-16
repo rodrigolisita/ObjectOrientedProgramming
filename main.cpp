6fb1a90643527cdbb1591dbd26801bf354459317
Rodrigo Lisita Ribera <rodrigolisita@gmail.com>
2024-09-16 11:26:36 -0300

    Refactoring. Moving the code to separate functions for each step.

--- main.cpp
+++ main.cpp
@@ -1,74 +1,83 @@
 #include <iostream>
 #include <string> // Include the string header
 
+void printMenu(){
+    std::cout << "=================== " << std::endl;
+    std::cout << "====== Menu ======= " << std::endl;
+    // 1 print help
+    std::cout << "1: Print help " << std::endl;
+
+    // 2 print exhange stats
+    std::cout << "2: Print exhange stats " << std::endl;
+        
+    // 3 make an offer
+    std::cout << "3: Make an offer " << std::endl;
+
+    // 4 make a bid
+    std::cout << "4: Make a bid " << std::endl;
+
+    // 5 print wallet
+    std::cout << "5: Print wallet " << std::endl;
+
+    // 6 continue
+    std::cout << "6: Continue " << std::endl;
+
+    std::cout << "=================== " << std::endl;
+    std::cout << "Type in 1-6: ";
+}
+
+std::string getUserOption(){
+    std::string userOption;
+    std::getline(std::cin, userOption);
+    return userOption;
+}
+
 void printChar(const std::string& inputString) {
     std::cout << "You entered " << inputString << std::endl;
 }
+
+void processUserOption(const std::string& userOption){
+
+    switch (userOption[0])
+    {
+    case '1':
+        printChar("1: Help - choose options from the menu\nand follow the on screen instructions.");
+        break;
+    case '2':
+        printChar("2: Print exhange stats");
+        break;
+    case '3':
+        printChar("3: Make an offer ");
+        break;
+    case '4':
+        printChar("4: Make a bid ");
+        break;
+    case '5':
+        printChar("5: Print wallet");
+        break;
+    case '6':
+        printChar("6: Continue");
+        break;                                                            
+    default:
+        std::cout << userOption << " is not a valid input. Please provide an input between 1 to 6 " << std::endl;
+        break;
+    }
+}
+
 
 int main()
 {
     while(true){
 
-        std::cout << "=================== " << std::endl;
-        // 1 print help
-        std::cout << "1: Print help " << std::endl;
+        printMenu(); //Menu options
 
-        // 2 print exhange stats
-        std::cout << "2: Print exhange stats " << std::endl;
-    
-        // 3 make an offer
-        std::cout << "3: Make an offer " << std::endl;
+        std::string userOption = getUserOption(); // Get the user input
 
-        // 4 make a bid
-        std::cout << "4: Make a bid " << std::endl;
-
-        // 5 print wallet
-        std::cout << "5: Print wallet " << std::endl;
-
-        // 6 continue
-        std::cout << "6: Continue " << std::endl;
-
-        std::cout << "=================== " << std::endl;
-        std::cout << "Type in 1-6: ";
-
-        //int userOption;
-        std::string userOption;
-        // std::cin >> userOption;
-        std::getline(std::cin, userOption);
-
-        switch (userOption[0])
-        {
-        case '1':
-            /* code */
-            printChar("Help - choose options from the menu\nand follow the on screen instructions.");
-            break;
-        case '2':
-            /* code */
-            printChar("2: Print exhange stats");
-            break;
-        case '3':
-            /* code */
-            printChar("3: Make an offer ");
-            break;
-        case '4':
-            /* code */
-            printChar("4: Make a bid ");
-            break;
-        case '5':
-            /* code */
-            printChar("5: Print wallet");
-            break;
-        case '6':
-            /* code */
-            printChar("6: Continue");
-            break;                                                            
-        default:
-            std::cout << userOption << " is not a valid input. Please provide an input between 1 to 6 " << std::endl;
-            break;
-        }
-        // std::cout << "You chose: " << userOption << std::endl;
+        processUserOption(userOption); // Process the user input
+       
+       
     }
-    std::cout << "=====================" << std::endl;
+    
     
     return 0;
 }
