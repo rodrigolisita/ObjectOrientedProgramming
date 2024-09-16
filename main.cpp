79f47408b739c802e56be50c53fd57dc0733ab14
Rodrigo Lisita Ribera <rodrigolisita@gmail.com>
2024-09-16 09:56:21 -0300

    Print the menu and the selected option with switch. Also handle non valid input (1 to 6) with the default case.

--- main.cpp
+++ main.cpp
@@ -2,23 +2,63 @@
 
 int main()
 {
-    // 1 print help
-    std::cout << "1: Print help " << std::endl;
+    while(true){
+        // 1 print help
+        std::cout << "1: Print help " << std::endl;
 
-    // 2 print exhange stats
-    std::cout << "2: Print exhange stats " << std::endl;
+        // 2 print exhange stats
+        std::cout << "2: Print exhange stats " << std::endl;
     
-    // 3 make an offer
-    std::cout << "3: Make an offer " << std::endl;
-    
-    // 4 make a bid
-    std::cout << "4: Make a bid " << std::endl;
-    
-    // 5 print wallet
-    std::cout << "5: Print wallet " << std::endl;
-    
-    // 6 continue
-    std::cout << "6: Continue " << std::endl;
+        // 3 make an offer
+        std::cout << "3: Make an offer " << std::endl;
 
+        // 4 make a bid
+        std::cout << "4: Make a bid " << std::endl;
+
+        // 5 print wallet
+        std::cout << "5: Print wallet " << std::endl;
+
+        // 6 continue
+        std::cout << "6: Continue " << std::endl;
+
+        std::cout << "=================== " << std::endl;
+        std::cout << "Type in 1-6: ";
+
+        int userOption;
+        std::cin >> userOption;
+
+        switch (userOption)
+        {
+        case 1:
+            /* code */
+            std::cout << "You chose: 1" << std::endl;
+            break;
+        case 2:
+            /* code */
+            std::cout << "You chose: 2" << std::endl;
+            break;
+        case 3:
+            /* code */
+            std::cout << "You chose: 3" << std::endl;
+            break;
+        case 4:
+            /* code */
+            std::cout << "You chose: 4" << std::endl;
+            break;
+        case 5:
+            /* code */
+            std::cout << "You chose: 5" << std::endl;
+            break;
+        case 6:
+            /* code */
+            std::cout << "You chose: 6" << std::endl;
+            break;                                                            
+        default:
+            std::cout << "Not a valid input. Please provide an input between 1 to 6 " << std::endl;
+            break;
+        }
+        // std::cout << "You chose: " << userOption << std::endl;
+    }
+    
     return 0;
 }
